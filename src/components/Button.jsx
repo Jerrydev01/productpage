b609194403d5f07b61c60b634814786e504442e0
zeus012345 <francisjerry360@gmail.com>
2022-06-17 00:59:36 +0100

    initial commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Button = ({ name }) => {
     return (
-        <div className="text-2xl mt-5 text-red-800">
+        <div className="text-2xl my-5 text-red-800">
             <button className="border-red-400 border-2  py-1 px-8 hover:bg-red-600 hover:text-white" type="submit">
                 {name}
             </button>
